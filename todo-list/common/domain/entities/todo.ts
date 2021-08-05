9e37280732a678444a2ce8742e12155079b4619f
David <deceived321@gmail.com>
2021-08-05 15:02:52 +0800

    Django rest authentication

--- todo-list/common/domain/entities/todo.ts
+++
@@ -1,9 +0,0 @@
-import { Base } from './base';
-
-export type TodoParams = {
-  title: string;
-  description: string;
-  completed?: boolean;
-};
-
-export type Todo = TodoParams & Base;
--- todoBackend/todo/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .serializers import TodoSerializer
-from rest_framework import viewsets
-from .models import Todo
-
-# Create your views here.
-class TodoView(viewsets.ModelViewSet):
-    serializer_class = TodoSerializer
-    queryset = Todo.objects.all().order_by('-created_at')
